d505f29c2cb27819e10b4788c6928b1d4af0c9d0
Kenton Poynter <kenton.poynter@stu.barren.kyschools.us>
2024-02-01 10:43:50 -0600

    Completed excercise

---
+++ script.js
@@ -0,0 +1,22 @@
+let warmHugs = "Hi, I'm Olaf and I like warm hugs."
+console.log(warmHugs.toUpperCase());
+warmHugs = warmHugs.replace("like", "love")
+console.log(warmHugs);
+let beenImpaled = "Oh, look at that. I've been impaled."
+console.log(beenImpaled.slice(18));
+const dotDotDot = "..."
+let skullBones = `I don't have a skull${dotDotDot}or bones`
+console.log(Math.PI);
+let randomNumber = Math.random() * 3
+randomNumber = Math.floor(randomNumber) + 1
+console.log(randomNumber)
+
+//Bonus
+
+console.log(" Let It Go!".toUpperCase().repeat(2).trim());
+
+let reindeers = "Reindeers are better than people."
+reindeers = reindeers.replace(/ /g,"_")
+console.log(reindeers);
+console.log(Math.sqrt(2));
+let newRandomNumber = 1 + Math.floor(Math.random() * 27)
